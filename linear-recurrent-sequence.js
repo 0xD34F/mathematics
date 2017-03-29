9d0f2acf0a73bd26aae1434c6b3942e6abb58b29
Victor <x89168419357@gmail.com>
2017-03-29 18:01:03 +0300

    LRS simplification

--- linear-recurrent-sequence.js
+++ linear-recurrent-sequence.js
@@ -6,28 +6,24 @@
  * a(n) = -2*a(n-2) + 3*a(n-3), a(0) = 4, a(1) = -5, a(2) = 1 : LRS({ 2: -2, 3: 3 }, [ 4, -5, 1 ])
  */
 var LRS = function(form, start) {
-    var mem = {};
+    var mem = start.slice(0);
 
     return function recursive(n) {
         if (n < 0) {
             return NaN;
         }
 
-        if (mem.hasOwnProperty(n)) {
+        if (mem.length > n) {
             return mem[n];
         }
 
         var s = 0;
 
-        if (n < start.length) {
-            s = start[n];
-        } else {
-            for (var i in form) {
-                s += recursive(n - i) * form[i];
-            }
+        for (var i in form) {
+            s += recursive(n - i) * form[i];
         }
 
-        mem[n] = s;
+        mem.push(s);
 
         return s;
     }
