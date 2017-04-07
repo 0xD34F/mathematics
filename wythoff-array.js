36b1f40610452837c772ab0aff6fe62684c6f038
Victor <x89168419357@gmail.com>
2017-04-07 16:06:23 +0300

    wythoff array - quicker recursion

--- wythoff-array.js
+++ wythoff-array.js
@@ -3,13 +3,13 @@ var wythoffArray = (function() {
     // золотое сечение
     var gr = (1 + Math.sqrt(5)) / 2;
 
-    function getElement(m, n) {
+    function getElement(m, n, previous) {
         if (n === 1) {
             return Math.floor(Math.floor(m * gr) * gr);
         } else if (n === 2) {
             return Math.floor(Math.floor(m * gr) * gr * gr);
         } else {
-            return getElement(m, n - 2) + getElement(m, n - 1);
+            return previous[n - 3] + previous[n - 2];
         }
     }
 
@@ -18,7 +18,7 @@ var wythoffArray = (function() {
         for (var i = 0; i < m; i++) {
             t[i] = new Array(n);
             for (var j = 0; j < n; j++) {
-                t[i][j] = getElement(i + 1, j + 1);
+                t[i][j] = getElement(i + 1, j + 1, t[i]);
             }
         }
 
