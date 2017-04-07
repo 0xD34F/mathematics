dd6f2607f2730fb3109f62c06faa32190081290e
Victor <x89168419357@gmail.com>
2017-04-07 16:24:56 +0300

    more compact code

--- wythoff-array.js
+++ wythoff-array.js
@@ -4,13 +4,9 @@ var wythoffArray = (function() {
     var gr = (1 + Math.sqrt(5)) / 2;
 
     function getElement(m, n, previous) {
-        if (n === 1) {
-            return Math.floor(Math.floor(m * gr) * gr);
-        } else if (n === 2) {
-            return Math.floor(Math.floor(m * gr) * gr * gr);
-        } else {
-            return previous[n - 3] + previous[n - 2];
-        }
+        return n > 1
+            ? previous[n - 2] + previous[n - 1]
+            : Math.floor(Math.floor((m + 1) * gr) * Math.pow(gr, n + 1));
     }
 
     return function(m, n) {
@@ -18,7 +14,7 @@ var wythoffArray = (function() {
         for (var i = 0; i < m; i++) {
             t[i] = new Array(n);
             for (var j = 0; j < n; j++) {
-                t[i][j] = getElement(i + 1, j + 1, t[i]);
+                t[i][j] = getElement(i, j, t[i]);
             }
         }
 
