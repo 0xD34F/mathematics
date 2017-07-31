965c7acc36d130eafe6a3513024e8da3098a5ea1
Victor <x89168419357@gmail.com>
2017-07-31 05:23:18 +0300

    complex numbers added

---
+++ complex.js
@@ -0,0 +1,50 @@
+﻿(function(factory) {
+    if (typeof window === 'undefined') {
+        module.exports = factory();
+    } else {
+        window.Complex = factory();
+    }
+}(function() {
+    function Complex(re, im) {
+        var c = Object.create(Complex.prototype);
+
+        if (re instanceof Complex) {
+            re = r.re;
+            im = r.im;
+        }
+
+        c.re = re;
+        c.im = im;
+
+        return c;
+    }
+
+    function complexParam(f) {
+        return function(re, im) {
+            return f.call(this, re instanceof Complex ? re : Complex(re, im));
+        };
+    }
+
+    Complex.prototype.equals = complexParam(function(c) {
+        return (this.re === c.re) && (this.im === c.im);
+    });
+
+    Complex.prototype.add = complexParam(function(c) {
+        return Complex(this.re + c.re, this.im + c.im);
+    });
+
+    Complex.prototype.sub = complexParam(function(c) {
+        return Complex(this.re - c.re, this.im - c.im);
+    });
+
+    Complex.prototype.mul = complexParam(function(c) {
+        return Complex(this.re * c.re - this.im * c.im, this.im * c.re + this.re * c.im);
+    });
+
+    Complex.prototype.div = complexParam(function(c) {
+        var d = c.re * c.re + c.im * c.im;
+        return Complex((this.re * c.re + this.im * c.im) / d, (this.im * c.re - this.re * c.im) / d);
+    });
+
+    return Complex;
+}));
