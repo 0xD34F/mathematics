4ed3b4eb95dea9316c7fe51cd6e1c124df01e97a
Victor <x89168419357@gmail.com>
2017-11-18 19:49:25 +0300

    mean values

---
+++ mean.js
@@ -0,0 +1,38 @@
+﻿(function(factory) {
+    if (typeof window === 'undefined') {
+        module.exports = factory();
+    } else {
+        window.mean = factory();
+    }
+}(function() {
+    // среднее арифметическое
+    function arithmetic(...values) {
+        var t = [].concat(...values);
+        return t.reduce((p, c) => p + c, 0) / t.length;
+    }
+
+    // среднее геометрическое
+    function geometric(...values) {
+        var t = [].concat(...values);
+        return Math.pow(t.reduce((p, c) => p * c, 1), 1 / t.length);
+    }
+
+    // среднее гармоническое
+    function harmonic(...values) {
+        var t = [].concat(...values);
+        return t.length / t.reduce((p, c) => p + 1 / c, 0);
+    }
+
+    // среднее квадратическое
+    function rms(...values) {
+        var t = [].concat(...values);
+        return Math.sqrt(t.reduce((p, c) => p + c * c, 0) / t.length);
+    }
+
+    return {
+        arithmetic,
+        geometric,
+        harmonic,
+        rms
+    };
+}));
